ec50e8cf1a98c799ea8ddffde1a5bdca2882a535
Yuki <rategyst@gmail.com>
2024-12-20 06:18:44 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    # !!ここではstrong parameterのuser_paramsを使わないとActiveModel::ForbittenAttributes errorが起こる
+    @user = User.new(user_params) # (params[:user])は on private
+    if @user.save
+      # flashを作成したらレイアウトをapplicationhtmlに記載
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # privateはweb経由で使ってほしくないもの(params[:user])を入れる。it is in controller limitedly so it works.
+  # インデントして強調　実行順が変わるのではなく単に重要な項目を見つけやすくする意図
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
