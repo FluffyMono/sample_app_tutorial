c25e361aac7b62e1cca93254fa010c01e5a50a4d
Yuki <rategyst@gmail.com>
2024-12-30 21:15:37 +0000

    css improvement

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,24 +1,21 @@
-// メニュー操作
-
-// トグルリスナーを追加してクリックをリッスンする
-//ページを開いた時
 document.addEventListener("turbo:load", function() {
-  //for mobile menu
-  let hamburger = document.querySelector("#hamburger");
-  if (hamburger){
+  // For mobile menu
+  const hamburger = document.querySelector("#hamburger");
+  if (hamburger) {
     hamburger.addEventListener("click", function(event) {
       event.preventDefault();
-      let menu = document.querySelector("#navbar-menu");
+      const menu = document.querySelector("#navbar-menu");
       menu.classList.toggle("collapse");
     });
   }
 
-  let account = document.querySelector("#account");
+  // For dropdown menu
+  const account = document.querySelector("#account");
   if (account) {
     account.addEventListener("click", function(event) {
       event.preventDefault();
-      let menu = document.querySelector("#dropdown-menu");
-      menu.classList.toggle("active");
+      const dropdownMenu = document.querySelector("#dropdown-menu");
+      dropdownMenu.classList.toggle("active");
     });
   }
 });
